fb63689f66bdefe0d85102ef47fd951a3d7b315c
cftgithub <chiafongt@gmail.com>
2020-07-09 17:59:03 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Card from "./components/Card";
+import Wrapper from "./components/Wrapper";
+import cards from "./cards.json";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    cards
+  };
+
+  clickCard = id => {
+    // Display the card is clicked
+    console.log(id);
+  }
+
+  render() {
+    return (
+      <Wrapper>
+        <h1 className="title">Clicky Game</h1>
+        {this.state.cards.map(card => (
+          <Card
+            clickCard={this.clickCard}
+            key={card.id}
+            id={card.id}
+            name={card.name}
+            image={card.image}
+            occupation={card.occupation}
+            location={card.location}
+          />
+        ))}
+
+      </Wrapper>
+    );
+  }
 }
 
 export default App;
