7e21b03e10bb6f7472045cb196a80c8330ec7bbc
cftgithub <chiafongt@gmail.com>
2020-07-09 18:06:52 -0600

    Updates

---
+++ precache-manifest.562f6dd5639b342e74c195f007789875.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a7376b738434d6bc77a6b65a85ab760",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "6ca72b4f1092639cb0d4",
+    "url": "/clickygame/static/css/main.b1b3aa41.chunk.css"
+  },
+  {
+    "revision": "6ce6f54f724085dc1c01",
+    "url": "/clickygame/static/js/2.93eb1781.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clickygame/static/js/2.93eb1781.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ca72b4f1092639cb0d4",
+    "url": "/clickygame/static/js/main.d9875b80.chunk.js"
+  },
+  {
+    "revision": "7122649522b9616ddb97",
+    "url": "/clickygame/static/js/runtime-main.71d67497.js"
+  }
+]);
